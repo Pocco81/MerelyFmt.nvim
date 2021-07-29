ee108dc3eaa83fb3195b2640bcf6a67e41789931
Pocco81 <pocco451@gmail.com>
2021-07-29 17:52:20 -0500

    added some formatters

---
+++ lua/merelyfmt/formatters/formatters.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.formatters = {
+    "luaformatter",
+    "luafmt",
+    "luaformat",
+    "stylua",
+    "yapf",
+    "autopep8",
+    "black",
+    "isort",
+    "docformatter",
+    "pyment",
+    "pydevf"
+}
+
+return M
