fd77c2777eb618a327adf361f23a43b8fba1e5a1
Pocco81 <pocco451@gmail.com>
2021-07-30 20:41:28 -0500

    passing nil to logic

--- lua/merelyfmt/main.lua
+++ lua/merelyfmt/main.lua
@@ -5,14 +5,13 @@ local formatter_prefix = "merelyfmt.core.formatter."
 
 function M.main(option, command_option)
     option = option or 0
-    command_option = command_option or "NONE"
 
     if (option == "install") then
         require(manager_prefix .. "install").install_formatter(command_option)
     elseif (option == "uninstall") then
         require(manager_prefix .. "uninstall").uninstall_formatter(command_option)
     elseif (option == "list") then
-        require(manager_prefix .. "list").list_formatters(command_option)
+        require(manager_prefix .. "list").list_formatters()
     elseif (option == "format") then
         require(formatter_prefix .. "format").format(command_option)
     else
